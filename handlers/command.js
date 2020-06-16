527a51a24a1b5876d2aa5e0f724b1809f5473b5c
cameronsawesome <34144877+cameronsawesome@users.noreply.github.com>
2020-06-16 00:05:55 -0400

    ddfsdfwdfdfwefwefwef

--- handlers/command.js
+++ handlers/command.js
@@ -9,5 +9,5 @@ module.exports = (bot) => {
       if (pull.config.aliases) pull.config.aliases.forEach(a => bot.aliases.set(a, pull.config.name));
     };
   };
-  ["images", "miscellaneous", "music", "moderation", "owner"].forEach(x => load(x));
+  ["miscellaneous"].forEach(x => load(x));
 };
